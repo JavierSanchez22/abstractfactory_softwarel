37bbe9d1113fea0e1621a0c897e17a3ac20eb936
JavierSanchez22 <fjsanchez@correo.url.edu.gt>
2025-10-23 09:21:07 -0600

    fix: added aversions to HTMLProducts

--- concreteProducts/HtmlProducts.ts
+++ concreteProducts/HtmlProducts.ts
@@ -1,38 +1,37 @@
 import * as AP from "../interfaces/AbstractProducts";
 
 export class HTMLHeading1 implements AP.Heading1 {
-  public convert(text: string): string {
-    return '<h1>${text}</h1>\n';
-  }
+    public convert(text: string): string {
+        return `<h1>${text}</h1>\n`;
+    }
 }
 
 export class HTMLHeading2 implements AP.Heading2 {
-  public convert(text: string): string {
-    return '<h2>${text}</h2>\n';
-  }
+    public convert(text: string): string {
+        return `<h2>${text}</h2>\n`;
+    }
 }
 
 export class HTMLHeading3 implements AP.Heading3 {
-  public convert(text: string): string {
-    return '<h3>${text}</h3>\n';
-  }
+    public convert(text: string): string {
+        return `<h3>${text}</h3>\n`;
+    }
 }
 
 export class HTMLParagraph implements AP.Paragraph {
-  public convert(text: string): string {
-    return '<p>${text}</p>\n\n';
-  }
+    public convert(text: string): string {
+        return `<p>${text}</p>\n\n`;
+    }
 }
 
 export class HTMLQuote implements AP.Quote {
-  public convert(text: string): string {
-    return '<blockquote>${text}</blockquote>\n\n';
-  }
+    public convert(text: string): string {
+        return `<blockquote>${text}</blockquote>\n\n`;
+    }
 }
 
-
 export class HTMLImage implements AP.Image {
-  public convert(url: string): string {
-    return '<img src="${url}" alt="image" />\n\n';
-  }
+    public convert(url: string): string {
+        return `<img src="${url}" alt="image" />\n\n`;
+    }
 }
