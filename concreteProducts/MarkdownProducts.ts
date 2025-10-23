965403603c7844d0737b1aca164fce44ef57fbf1
Christian Villegas <chrivill02777@gmail.com>
2025-10-22 18:45:51 -0600

    feat(products): implement markdown heading converters (H1, H2,  H3)

---
+++ concreteProducts/MarkdownProducts.ts
@@ -0,0 +1,21 @@
+import * as AP from "../Interfaces/AbstractProducts"; 
+
+
+export class MarkDownHeading1 implements AP.Heading1 {
+  public convert(text: string): string {
+    return `# ${text}\n`; 
+  } 
+}
+
+export class MarkDownHeading2 implements AP.Heading2 {
+  public convert(text: string): string {
+    return `## ${text}\n`;
+  }
+}
+
+export class MarkDownHeading3 implements AP.Heading3 {
+  public convert(text: string): string {
+    return `### ${text}\n`;
+  }
+} 
+
